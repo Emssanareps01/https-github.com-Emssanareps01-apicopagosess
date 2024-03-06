9410c34dc5d0ae1575d395b551a9ba6606402e1c
Emssanareps01 <informativoemssanar@gmail.com>
2024-03-06 16:06:34 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -3,65 +3,70 @@ import { useState } from 'react';
 import Header from '@components/Header';
 import Footer from '@components/Footer';
 
+const obtenerToken = async () => {
+  try {
+    const credentials = {
+      username: '1lerkbvv9bblni5kvs3ffiovtd',
+      password: '1qh8dk63cerdj0ptca1oh1hbsjepurs6qak5vh420cntm2k92i13',
+    };
+
+    const response = await fetch('https://cross-gateway.conexia.com/uat/login/api', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(credentials),
+    });
+
+    const responseData = await response.json();
+
+    if (responseData.access_token) {
+      return responseData.access_token;
+    } else {
+      throw new Error('Error al obtener el token.');
+    }
+  } catch (error) {
+    throw new Error(`Error al obtener el token: ${error.message}`);
+  }
+};
+
+const consultarCopagos = async (token, tipoDocumento, numeroDocumento, fechaNacimiento) => {
+  try {
+    const parametrosConsulta = {
+      TipoDocumento: tipoDocumento,
+      NumeroDocumento: numeroDocumento,
+      fechaNacimiento: fechaNacimiento,
+    };
+
+    const copagosResponse = await fetch('https://emssanar-gateway.conexia.com/uat/api/integraciones/consultar-copagos-afiliado', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(parametrosConsulta),
+    });
+
+    const copagosData = await copagosResponse.json();
+    return copagosData;
+  } catch (error) {
+    throw new Error(`Error al consultar copagos: ${error.message}`);
+  }
+};
+
 export default function Home() {
   const [response, setResponse] = useState({});
-  const [token, setToken] = useState('');
   const [tipoDocumento, setTipoDocumento] = useState('CC');
   const [numeroDocumento, setNumeroDocumento] = useState('5544728');
   const [fechaNacimiento, setFechaNacimiento] = useState('1939-11-04');
 
-  const obtenerToken = async () => {
+  const handleConsultarCopagos = async () => {
     try {
-      const credentials = {
-        username: '1lerkbvv9bblni5kvs3ffiovtd',
-        password: '1qh8dk63cerdj0ptca1oh1hbsjepurs6qak5vh420cntm2k92i13',
-      };
-
-      const response = await fetch('https://cross-gateway.conexia.com/uat/login/api', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(credentials),
-      });
-
-      const responseData = await response.json();
-
-      if (responseData.access_token) {
-        setToken(responseData.access_token);
-        setResponseText('Token obtenido correctamente.');
-      } else {
-        setResponseText('Error al obtener el token.');
-      }
-    } catch (error) {
-      setResponseText(`Error al obtener el token: ${error.message}`);
-    }
-  };
-
-  const consultarCopagos = async () => {
-    try {
-      // Obtener un nuevo token en cada consulta
-      await obtenerToken();
-
-      const parametrosConsulta = {
-        TipoDocumento: tipoDocumento,
-        NumeroDocumento: numeroDocumento,
-        fechaNacimiento: fechaNacimiento,
-      };
-
-      const copagosResponse = await fetch('https://emssanar-gateway.conexia.com/uat/api/integraciones/consultar-copagos-afiliado', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`,
-        },
-        body: JSON.stringify(parametrosConsulta),
-      });
-
-      const copagosData = await copagosResponse.json();
+      const token = await obtenerToken();
+      const copagosData = await consultarCopagos(token, tipoDocumento, numeroDocumento, fechaNacimiento);
       setResponse(copagosData);
     } catch (error) {
-      setResponseText(`Error al consultar copagos: ${error.message}`);
+      console.error(error.message);
     }
   };
 
@@ -96,7 +101,7 @@ export default function Home() {
           </label>
         </form>
 
-        <button onClick={consultarCopagos}>Consultar Copagos</button>
+        <button onClick={handleConsultarCopagos}>Consultar Copagos</button>
 
         {/* Mostrar detalles de la respuesta en elementos div */}
         {Object.keys(response).length > 0 && (
