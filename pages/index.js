c44c021ce7cd4b77d65af8f1f2c5208c59558002
Emssanareps01 <informativoemssanar@gmail.com>
2024-03-06 08:35:43 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,11 +9,11 @@ export default function Home() {
   const handleButtonClick = async () => {
     try {
       const credentials = {
-        username: '6tp2r4in2naoiq6p7a07m01laa',
-        password: '1epguv5pso4m2sd2523tgrn6p1qv1l0m751ebp0amntneekduhs2',
+        username: '1lerkbvv9bblni5kvs3ffiovtd',
+        password: '1qh8dk63cerdj0ptca1oh1hbsjepurs6qak5vh420cntm2k92i13',
       };
 
-      const response = await fetch('https://cross-gateway.conexia.com/login/api', {
+      const response = await fetch('https://cross-gateway.conexia.com/uat/login/api', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
